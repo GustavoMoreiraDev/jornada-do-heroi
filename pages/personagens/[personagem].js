9a1000b13a2b16f553c2f94ce592c6a597e0b614
gustavo <gustavosantosmoreira52@gmail.com>
2023-10-28 15:54:35 -0300

    :up:

--- pages/personagens/[personagem].js
+++ pages/personagens/[personagem].js
@@ -1,12 +1,16 @@
 import Head from "next/head";
+import { UseData } from "@/context/data";
 import { UsePerson } from "@/context/personagens";
 
 import Banner from "@/components/personagem/banner";
+import Loading from "@/components/custom/loading";
 
 export default function Personagem() {
-  
+
+  const { loading } = UseData();
   const { person } = UsePerson();
 
+  if (loading) { return <Loading />};
   return (
     <>
         <Head>
--- pages/personagens/index.js
+++ pages/personagens/index.js
@@ -1,11 +1,18 @@
 import Head from "next/head";
+import { UseData } from "@/context/data";
 
 import Header from "@/components/header";
 import Navigation from "@/components/custom/navigation";
 import Column from "@/components/custom/column";
 import Card from "@/components/custom/card";
 import Search from "@/components/custom/search";
+import Loading from "@/components/custom/loading";
 export default function PersonagensPage() {
+
+  const { loading } = UseData();
+
+  if (loading) { return <Loading />};
+  
   return (
     <>
       <Head>
